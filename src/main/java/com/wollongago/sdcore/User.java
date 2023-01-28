8740b62d82c94f0f11ad498c5b1e2457483393c6
malkmaneth <miguelflores.preface@gmail.com>
2023-01-28 16:28:31 +0800

    fixed: can now retrieve the collections

--- src/main/java/com/wollongago/sdcore/User.java
+++ src/main/java/com/wollongago/sdcore/User.java
@@ -1,4 +1,4 @@
-package com.model;
+package com.wollongago.sdcore;
 
 
 import org.springframework.data.annotation.Id;
--- src/main/java/com/wollongago/sdcore/UserController.java
+++ src/main/java/com/wollongago/sdcore/UserController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.wollongago.sdcore;
 
 import java.util.List;
   
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.model.User;
-import com.repository.UserRespository;
 
 @RestController
 public class UserController {
