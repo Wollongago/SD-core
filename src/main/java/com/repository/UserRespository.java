96bf4deddfb38f40af3ce1964ebbdbdc512d8b6a
malkmaneth <miguelflores.preface@gmail.com>
2023-02-09 10:47:52 +0800

    add devtools and rest dependencies

--- src/main/java/com/repository/UserRespository.java
+++ src/main/java/com/repository/UserRespository.java
@@ -2,8 +2,10 @@ package com.repository;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.model.User;
+@RepositoryRestResource(path="users")
 public interface UserRespository extends MongoRepository<User, String> {
     // Optional<User> findByUsername(String username);
   
