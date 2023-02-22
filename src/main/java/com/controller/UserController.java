c85a289f4f4052dc86f3dd0e3e75cfc3e094473b
malkmaneth <miguelflores.preface@gmail.com>
2023-02-22 13:03:19 +0800

    refactor: user methods

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,22 +33,16 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/add")
-    public User addUser(@RequestBody User user) {
-        return userService.addUser(user);
-    }
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
+        return userService.deleteUser(id);
+    }   
 
-    @GetMapping("/delete")
-    public String deleteUser(@RequestBody User user) {
-        return userService.deleteUser(user);
-    }
-
-    @GetMapping("/update")
-    public User updateUser(@RequestBody User user) {
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
         return userService.updateUser(user);
+        
     }
-
-    
 
 
     
