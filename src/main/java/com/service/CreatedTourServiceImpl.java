0066c92a8a63128e01e4794b8c5b7173381889ea
malkmaneth <miguelflores.preface@gmail.com>
2023-03-06 18:39:45 +0800

    feat: CreatedTour service class and impl

---
+++ src/main/java/com/service/CreatedTourServiceImpl.java
@@ -0,0 +1,61 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import com.model.CreatedTour;
+import com.repository.CreatedTourRepository;
+
+@Service
+public class CreatedTourServiceImpl implements CreatedTourService {
+
+    @Autowired
+    private CreatedTourRepository createdTourRepo;
+
+    // create tour
+    public List<CreatedTour> getAllCreatedTours() {
+        return createdTourRepo.findAll();
+    }
+
+    
+    public Optional<CreatedTour> getCreatedTourById(String id) {
+        return createdTourRepo.findById(id);
+    }
+
+    
+    public CreatedTour createTour(CreatedTour createdTour) {
+        return createdTourRepo.save(createdTour);
+    }
+
+    
+    public CreatedTour updateCreatedTour(String id, CreatedTour createdTour) {
+        Optional<CreatedTour> existingCreatedTour = createdTourRepo.findById(id);
+        if (existingCreatedTour.isPresent()) {
+            createdTour.setId(id);
+            return createdTourRepo.save(createdTour);
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    
+    public String deleteCreatedTour(String id) {
+        if (createdTourRepo.existsById(id)) {
+            createdTourRepo.deleteById(id);
+            return createdTourRepo.existsById(id) ? "Tour deletion failed" : "Tour Deleted";
+        } else {
+            return "Tour Not Found";
+        }
+    }
+
+    
+    public List<CreatedTour> getCreatedToursByTourGuideId(String tourGuideId) {
+        return createdTourRepo.findByTourGuideId(tourGuideId);
+    }
+}
+
