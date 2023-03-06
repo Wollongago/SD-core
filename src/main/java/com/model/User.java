caffce549d7f57e16e3ed26c8939921f719b6fd9
malkmaneth <miguelflores.preface@gmail.com>
2023-03-06 18:38:19 +0800

    feat: new attributes for user

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,5 +1,8 @@
 package com.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,18 +16,32 @@ public class User {
 
   public String name;
   public String email;
-
+  
   @JsonIgnore
   private String password;
+  
+  private boolean isTourProvider;
+
+  // For tourists
+  private List<TourBooking> bookings;
+
+  // For tour guides
+  private List<CreatedTour> createdTours;
 
   public User() {
   }
 
-  public User(String name, String email, String password) {
+  public User(String name, String email, String password, boolean isTourProvider) {
     super();
     this.name = name;
     this.email = email;
     this.password = password;
+    this.isTourProvider = isTourProvider;
+    if (isTourProvider) {
+      this.createdTours = new ArrayList<CreatedTour>();
+    } else {
+      this.bookings = new ArrayList<TourBooking>();
+    }
   }
 
   public String getId() {
@@ -57,5 +74,34 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public boolean isTourProvider() {
+    return isTourProvider;
+  }
+
+  public void setTourGuide(boolean isTourProvider) {
+    this.isTourProvider = isTourProvider;
+    if (isTourProvider) {
+      this.createdTours = new ArrayList<CreatedTour>();
+    } else {
+      this.bookings = new ArrayList<TourBooking>();
+    }
+  }
+
+  public List<TourBooking> getBookings() {
+    return bookings;
+  }
+
+  public void setBookings(List<TourBooking> bookings) {
+    this.bookings = bookings;
+  }
+
+  public List<CreatedTour> getCreatedTours() {
+    return createdTours;
+  }
+
+  public void setCreatedTours(List<CreatedTour> createdTours) {
+    this.createdTours = createdTours;
   }
 }
